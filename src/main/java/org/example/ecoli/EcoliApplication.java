f87a8b2174257c7d8ca374284ca516ce49c806f1
Andy Wilkinson <andy.wilkinson@broadcom.com>
2024-06-13 09:42:25 +0100

    Break cycle with lazy ReporterMetrics

--- src/main/java/org/example/ecoli/EcoliApplication.java
+++ src/main/java/org/example/ecoli/EcoliApplication.java
@@ -1,17 +1,22 @@
 package org.example.ecoli;
 
+import java.util.function.Supplier;
+
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.util.function.SingletonSupplier;
+
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.tracing.Span;
 import io.micrometer.tracing.Tracer;
 import io.prometheus.client.exemplars.tracer.common.SpanContextSupplier;
 import zipkin2.codec.BytesEncoder;
 import zipkin2.reporter.AsyncReporter;
+import zipkin2.reporter.ReporterMetrics;
 import zipkin2.reporter.Sender;
 import zipkin2.reporter.metrics.micrometer.MicrometerReporterMetrics;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EcoliApplication {
@@ -20,9 +25,9 @@ public class EcoliApplication {
 	}
 
 	@Bean
-	AsyncReporter<zipkin2.Span> spanReporter(Sender sender, BytesEncoder<zipkin2.Span> encoder, MeterRegistry meterRegistry) {
+	AsyncReporter<zipkin2.Span> spanReporter(Sender sender, BytesEncoder<zipkin2.Span> encoder, ObjectProvider<MeterRegistry> meterRegistry) {
 		return AsyncReporter.builder(sender)
-			.metrics(MicrometerReporterMetrics.create(meterRegistry))
+			.metrics(new LazyReporterMetrics(SingletonSupplier.of(() -> MicrometerReporterMetrics.create(meterRegistry.getObject()))))
 			.build(encoder);
 	}
 
@@ -64,4 +69,56 @@ public class EcoliApplication {
 			return this.tracer.currentSpan();
 		}
 	}
+	
+	static class LazyReporterMetrics implements ReporterMetrics {
+
+		private final Supplier<ReporterMetrics> reporterMetrics;
+
+		public LazyReporterMetrics(Supplier<ReporterMetrics> reporterMetrics) {
+			this.reporterMetrics = reporterMetrics;
+		}
+
+		@Override
+		public void incrementMessages() {
+			this.reporterMetrics.get().incrementMessages();
+		}
+
+		@Override
+		public void incrementMessagesDropped(Throwable cause) {
+			this.reporterMetrics.get().incrementMessagesDropped(cause);
+			
+		}
+
+		@Override
+		public void incrementSpans(int quantity) {
+			this.reporterMetrics.get().incrementSpans(quantity);
+		}
+
+		@Override
+		public void incrementSpanBytes(int quantity) {
+			this.reporterMetrics.get().incrementSpanBytes(quantity);
+		}
+
+		@Override
+		public void incrementMessageBytes(int quantity) {
+			this.reporterMetrics.get().incrementMessageBytes(quantity);
+		}
+
+		@Override
+		public void incrementSpansDropped(int quantity) {
+			this.reporterMetrics.get().incrementSpansDropped(quantity);
+		}
+
+		@Override
+		public void updateQueuedSpans(int update) {
+			this.reporterMetrics.get().updateQueuedSpans(update);
+		}
+
+		@Override
+		public void updateQueuedBytes(int update) {
+			this.reporterMetrics.get().updateQueuedBytes(update);
+		}
+
+	}
+
 }
